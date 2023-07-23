cbbf8ddf6dca4cad7be35be84af87332adec5987
Muhammad Bilal Ahmad <111138242+bilalattari407@users.noreply.github.com>
2023-07-23 17:24:43 +0500

    Class => Object || Instance
    Class => Object || Instance

---
+++ index.dart
@@ -0,0 +1,13 @@
+void main() {
+  // var functionValue = printFunction();
+  // print(functionValue);
+  print(sumOfValues(50, 94));
+}
+
+printFunction() {
+  List myList = [];
+  // myList.forEach((e) =>);
+  return {"data": "Hello World"};
+}
+
+sumOfValues(a, b) => {a + b};
